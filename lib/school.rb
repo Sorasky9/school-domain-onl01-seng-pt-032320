626e9f66659c1b3299daba8699be491f47369e0c
Malcom Fetus <sora.vista@gmail.com>
2020-04-12 19:45:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
+require 'pry'
 # code here!
+class School
+
+attr_accessor :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, grade)
+  @name = name
+  if roster[grade]
+    roster[grade] << name
+
+  else
+    roster[grade]= [name]
+
+  end
+  roster
+  end
+
+  def grade(grade)
+    roster[grade]
+
+  end
+
+def student(student)
+  #binding.pry
+  @student = student
+
+end
+
+def sort
+@roster.each do |grade, value|
+  value.sort!
+
+
+end
+
+end
+
+end
