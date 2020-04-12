7727065a6a3771b757d688c0cf6612ecf1aeb6ac
Malcom Fetus <sora.vista@gmail.com>
2020-04-12 18:37:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
+require 'pry'
 # code here!
+class School
+
+attr_accessor :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, grade)
+  @name = name
+  if roster[grade]
+    roster[grade] << name
+
+  else
+    roster[grade]=[name]
+
+  end
+  roster
+  end
+
+  def grade(grade)
+    @grade = grade
+    roster[10]
+  end
+
+def sort(sort)
+  @sort = sort
+
+  binding.pry
+
+end
+
+
+
+end
